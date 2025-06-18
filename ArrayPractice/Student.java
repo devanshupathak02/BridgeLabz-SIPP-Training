882f60b2bce8785671e50c14e2e586d1eea50337
devanshupathak02 <devanshupathak2003@gmail.com>
2025-06-18 09:56:20 +0530

    Control flow added

---
+++ ArrayPractice/Student.java
@@ -0,0 +1,13 @@
+package ArrayPractice;
+import java.util.*;
+
+public class Student {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<10;i++){
+            int age=sc.nextInt();
+            if(age >=18) System.out.println("can vote");
+            else System.out.println("cannot vote");
+        }    
+    }
+}
